766ef35be686150e62b918cb8150e2c150d402e3
Tunç İrkörücü <tuncirk08@gmail.com>
2026-03-04 21:17:59 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("Simple Calculator")
+
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+print("Sum:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
